36131bbbbfce8f584b21f7be85a315255774d274
guaiguaide1 <2121645611@qq.com>
2021-06-27 15:28:40 +0800

    version 4

--- src/component/TeaTable.vue
+++ src/component/TeaTable.vue
@@ -30,13 +30,19 @@
     <el-row>
       <el-form :inline="true" :model="formInline" class="select-form">
         <el-form-item label="教工号：">
-          <el-input v-model="formInline.id" ></el-input>
+          <el-input v-model="formInline.tno" ></el-input>
         </el-form-item>
         <el-form-item label="姓名：">
-          <el-input v-model="formInline.name" ></el-input>
+          <el-input v-model="formInline.tname" ></el-input>
         </el-form-item>
         <el-form-item label="职称：">
           <el-input v-model="formInline.title" ></el-input>
+        </el-form-item>
+        <el-form-item label="性别：">
+          <el-input v-model="formInline.tsex" ></el-input>
+        </el-form-item>
+        <el-form-item label="密码：">
+          <el-input v-model="formInline.pass" type="password"></el-input>
         </el-form-item>
         <el-form-item label="入职时间：">
           <el-input v-model="formInline.hireDate" ></el-input>
@@ -45,7 +51,7 @@
           <el-input v-model="formInline.root" ></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">查询</el-button>
+          <el-button type="primary" @click="queryTeacher">查询</el-button>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="addTeacher">添加教师信息</el-button>
@@ -63,7 +69,7 @@
         </el-table-column>
         <el-table-column
             fixed
-            prop="tid"
+            prop="tno"
             label="教工号"
             header-align="center"
             width="150">
@@ -87,7 +93,7 @@
             width="120">
         </el-table-column>
         <el-table-column
-            prop="privilege"
+            prop="root"
             label="权限"
             header-align="center"
             width="200">
@@ -113,10 +119,24 @@
             width="120">
           <template slot-scope="scope">
             <el-button
-                @click.native.prevent="updateRow(scope.$index, tableData)"
+                @click.native.prevent="updatetitle(scope.$index, tableData)"
                 type="text"
                 size="small">
-              修改
+              修改职称
+            </el-button>
+          </template>
+        </el-table-column>
+        <el-table-column
+            fixed="right"
+            label="操作"
+            header-align="center"
+            width="120">
+          <template slot-scope="scope">
+            <el-button
+                @click.native.prevent="updateroot(scope.$index, tableData)"
+                type="text"
+                size="small">
+              修改权限
             </el-button>
           </template>
         </el-table-column>
@@ -134,41 +154,41 @@ export default {
   data(){
     return{
       tableData: [{
-        tid: '10001',
+        tno: '10001',
         tname: '庄朝晖',
         title: '副教授',
         hireDate:"2010-09",
-        privilege: 't',
+        root: 't',
       }, {
-        tid: '10006',
+        tno: '10006',
         tname: '严严',
         title: '教授',
         hireDate:"2010-09",
-        privilege: 't',
+        root: 't',
       }, {
-        tid: '10008',
+        tno: '10008',
         tname: '高淳县',
         title: '助理教授',
         hireDate:"2010-09",
-        privilege: 't',
+        root: 't',
       }, {
-        tid: '10020',
+        tno: '10020',
         tname: '林紫雨',
         title: '副教授',
         hireDate:"2010-09",
-        privilege: 't',
+        root: 't',
       }, {
-        tid: '10033',
+        tno: '10033',
         tname: '吴素贞',
         title: '副教授',
         hireDate:"2010-09",
-        privilege: 't',
+        root: 't',
       },  {
-        tid: '10088',
+        tno: '10088',
         tname: '林文水',
         title: '教授',
         hireDate:"2010-09",
-        privilege: 't',
+        root: 't',
       }],
       navList:[
         {name:'/course',navItem:'课程信息管理'},
@@ -177,11 +197,13 @@ export default {
         {name:'/admin',navItem:'管理员信息'}
       ],
       formInline: {
-        id: '',
-        name: '',
+        tno: '',
+        tname: '',
+        tsex:'',
         title:'',
+        pass:'123456',
         hireDate:'',
-        root:''
+        root:'t'
       },
       multipleSelection:0
 
@@ -195,30 +217,125 @@ export default {
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
     },
+    queryTeacher(){
+      this.$axios({//输入教工号 or 教师名 or 职称,可查询出对应的教师信息
+        method:'get',
+        url:'http://150.158.171.212:8080/getscore?tno=' + this.formInline.tno+
+            '&tname='+this.formInline.tname+'&title'+this.formInline.title,
+      }).then(response => { //返回tno, tname, title,hireDate,root
+        console.log(response.data) //在控制台中打印其data部分内容
+        var res = response.data;//
+        if (res != null){
+          this.tableData = res
+        }
+        else{
+          this.$alert('查询教师信息失败', {
+            confirmButtonText: '确定',
+            callback: action => {
+              this.$message({
+                type: 'info',
+                message: `action: ${action}`
+              });
+            }
+          })
+        }
+      })
+    },
     deleteRow(index, rows) {
       rows.splice(index, 1);
     },
-    updateRow(index, rows) {
-      console.log(this.form.username);
-      console.log("submit!");
-      console.log(this.radio);
-      this.$axios({
+    updatetitle(index, rows) {//修改教师职称
+      var newstitle;
+      this.$prompt('请输入此教师的新职称', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        // inputPattern:/^\d{0,2}$/,
+        // inputErrorMessage: '成绩格式不正确'
+      }).then(({ value }) => {
+        console.log("修改后的职称为：");
+        console.log(value);
+        newstitle=value;
+        this.$message({
+          type: 'success',
+          message: '新的职称: ' + value,
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '取消输入'
+        });
+      });
+
+      this.$axios({//后端在教师表中更新
         method:'post',
-        url:'http://150.158.171.212:8080/checklogin',
+        url:'http://150.158.171.212:8080/updatescore',//这里需要修改接口
         data:{	//按照对象的格式去组织data，key-value形式
-          "stu":this.form.username,
-          "pass":this.form.password,
-          "permissionId":this.radio,
-        }
+          "tno":this.formInline[index].tid,
+          "title":newstitle
+        },
       }).then(response => { //这里的response是通过get方法请求得到的内容
-        //在这里添加对于数据的操作
-        console.log(11111111)
-        //经常性的操作如下
-        console.log(response.data) //在控制台中打印其data部分内容
+        console.log(response.data);
       })
     },
-    onSubmit(){},
-    addTeacher(){},
+    updateroot(index, rows) {//修改教师的权限
+      var newroot;
+      this.$prompt('请输入此教师的新权限', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        // inputPattern:/^\d{0,2}$/,
+        // inputErrorMessage: '成绩格式不正确'
+      }).then(({ value }) => {
+        console.log("修改后的权限为：");
+        console.log(value);
+        newroot=value;
+        this.$message({
+          type: 'success',
+          message: '新的权限: ' + value,
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '取消输入'
+        });
+      });
+
+      this.$axios({//后端在教师表中更新
+        method:'post',
+        url:'http://150.158.171.212:8080/updatescore',//这里需要修改接口
+        data:{	//按照对象的格式去组织data，key-value形式
+          "tno":this.formInline[index].tid,
+          "root":newroot
+        },
+      }).then(response => { //这里的response是通过get方法请求得到的内容
+        console.log(response.data);
+      })
+    },
+    addTeacher(){
+      this.$axios({//添加学生成绩信息,不需要返回任何信息
+        method:'post',
+        url:'http://150.158.171.212:8080/updatescore',
+        data:{	//按照对象的格式去组织data，key-value形式
+          "cno":this.Common.courseId,
+          "sno":this.formInline.sid,
+          "score":this.formInline.score,
+        },
+      }).then(response => { //这里的response是通过get方法请求得到的内容
+        console.log("添加学生的成绩不需要返回任何信息");
+        console.log(response.data);
+        if(response.data == 1){
+          //录入成功
+          this.$message('录入成功');
+          this.formInline.sid='';
+          this.formInline.score='';
+        }
+        else{
+          //录入失败
+          this.$message('录入失败');
+          this.formInline.sid='';
+          this.formInline.score='';
+        }
+      })
+    },
     toggleSelection(rows) {
       if (rows) {
         rows.forEach(row => {
